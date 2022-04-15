cf655e8c5d1ee056cf28ec006d801a60356b1e71
MikhailTapio <81358657+MikhailTapio@users.noreply.github.com>
2022-04-15 11:43:13 +0800

    Port to fabric 1.14

---
+++ src/main/java/committee/nova/quickplant/QuickPlant.java
@@ -0,0 +1,10 @@
+package committee.nova.quickplant;
+
+import net.fabricmc.api.ModInitializer;
+
+public class QuickPlant implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
