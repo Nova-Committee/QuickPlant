45ee337ff8bbe9cf254ea59a563ca84f9d8cdaf7
MikhailTapio <mikhailtapio13468@163.com>
2022-04-21 11:03:10 +0800

    Update quilt

---
+++ src/main/java/committee/nova/quickplant/QuickPlant.java
@@ -0,0 +1,11 @@
+package committee.nova.quickplant;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class QuickPlant implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
